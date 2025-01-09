e4785029ddc4e02490f78250fd908f91acbf103e
jadchahine111 <jadalichahine@gmail.com>
2025-01-09 15:27:49 +0200

    Setup shadcn and init project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import About from './components/About'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Pricing from './components/Pricing'
+import Contact from './components/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <main className="min-h-screen">
+        <Navigation />
+        <Hero />
+        <About />
+        <Experience />
+        <Projects />
+        <Pricing />
+        <Contact />
+      </main>
   )
 }
 
 export default App
+
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Navigation
